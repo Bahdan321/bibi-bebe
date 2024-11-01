7321d5cd771f2384ce1eae55b3e5355be48abded
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-11-01 19:41:30 +0200

    adding: task card component

--- app/(protected)/home.tsx
+++ app/(protected)/home.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Svg, Circle } from 'react-native-svg';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+import { TaskCard } from '~/components/TaskCard';
 
 export default function Home() {
     const [progress, setProgress] = React.useState(78);
@@ -14,20 +15,34 @@ export default function Home() {
     }
     return (
         <SafeAreaView className='bg-slate-50' style={{ marginTop: hp('3') }}>
-            <View className='absolute top-0 left-0 p-8'>
-                <View className='flex flex-col'>
-                    <Text className='text-black font-semibold' style={{ fontSize: wp('5') }}>Приветули User</Text>
-                    <View className='flex flex-row justify-items-center'>
-                        <View className='flex flex-col'>
-                            <Text className='text-black font-extrabold' style={{ fontSize: wp('12') }}>Ваши</Text>
-                            <Text className='text-black font-extrabold' style={{ fontSize: wp('12') }}>Задачи</Text>
-                        </View>
-                        <View className='justify-center pl-14'>
-                            <Avatar alt="Zach Nugent's Avatar" style={{ height: 100, width: 100 }}>
-                                <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
-                            </Avatar>
+            <View className='flex flex-col'>
+                {/* 
+                Приветствие пользователя и его аватар
+                */}
+                <View className='absolute top-0 left-0 p-8 rounded-3xl'>
+                    <View className='flex flex-col'>
+                        <Text className='text-black font-semibold' style={{ fontSize: wp('5') }}>Приветули User</Text>
+                        <View className='flex flex-row justify-items-center'>
+                            <View className='flex flex-col'>
+                                <Text className='text-black font-extrabold' style={{ fontSize: wp('12') }}>Ваши</Text>
+                                <Text className='text-black font-extrabold' style={{ fontSize: wp('12') }}>Задачи</Text>
+                            </View>
+                            <View className='justify-center' style={{ paddingLeft: wp('16') }}>
+                                <Avatar alt="Zach Nugent's Avatar" style={{ height: 100, width: 100 }}>
+                                    <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
+                                </Avatar>
+                            </View>
                         </View>
                     </View>
+                </View>
+                {/* 
+                Блоки с задачами пользователя
+                */}
+                <View className='flex flex-col justify-items-center mt-60'>
+                    <ScrollView>
+                        <TaskCard themeText='Важно' onNavigate={() => { }} onAddTask={() => { }} />
+                        <TaskCard themeText='Не важно' onNavigate={() => { }} onAddTask={() => { }} />
+                    </ScrollView>
                 </View>
             </View>
         </SafeAreaView >
---
+++ components/TaskCard.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, Alert, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Entypo from '@expo/vector-icons/Entypo';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Progress } from '~/components/ui/progress';
+
+interface TaskCardProps {
+    themeText: string;
+    onNavigate: () => void;
+    onAddTask: () => void;
+}
+
+const getRandomGradient = () => {
+    const colors = ["red", "blue", "white", "black", "orange", "green", "purple", "gray"];
+
+    const getRandomInt = (min: number, max: number) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    const getRandomColors = (colorsArray: string[]) => {
+        const numColors = getRandomInt(2, 2);
+        const selectedColors = new Set();
+
+        while (selectedColors.size < numColors) {
+            const randomIndex = getRandomInt(0, colorsArray.length - 1);
+            selectedColors.add(colorsArray[randomIndex]);
+        }
+
+        return Array.from(selectedColors);
+    };
+
+    return getRandomColors(colors);
+};
+
+
+
+export const TaskCard: React.FC<TaskCardProps> = ({ themeText, onNavigate, onAddTask }) => {
+    return (
+        <View className='' style={{ padding: wp('1'), }}>
+            <LinearGradient
+                colors={getRandomGradient()}
+                className=''
+                style={{
+                    height: hp('50'), paddingLeft: wp('6'), paddingTop: wp('12'),
+                    borderTopLeftRadius: 60,
+                    borderTopRightRadius: 60,
+                    borderBottomLeftRadius: 60,
+                    borderBottomRightRadius: 60,
+
+                }}
+            >
+                <View className='flex flex-col'>
+                    <View className='flex flex-row'>
+                        <Text className='text-white font-extrabold text-center' style={{ fontSize: wp('12') }}>{themeText}</Text>
+                    </View>
+                </View>
+                <View className='absolute bottom-0 left-0'>
+                    <TouchableOpacity
+                        className='rounded-full bg-blue-500 justify-center'
+                        style={{ height: hp('15'), width: hp('15'), margin: wp('3') }}
+                        onPress={onNavigate}
+                    >
+                        <AntDesign name="ellipsis1" size={wp('16')} color="white" className='text-center' />
+                    </TouchableOpacity>
+                </View>
+                <View className='absolute bottom-0 right-0'>
+                    <TouchableOpacity
+                        className='rounded-full bg-blue-200 justify-center'
+                        style={{
+                            height: hp('15'), width: hp('15'), margin: wp('3')
+                        }}
+                        onPress={onAddTask}
+                    >
+                        <Entypo name="plus" size={wp('16')} color="black" className='text-center' />
+                    </TouchableOpacity>
+                </View>
+            </LinearGradient>
+        </View >
+    );
+};
