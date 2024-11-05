627efa6ec088636eaaceccd7dec91ad3efab0aeb
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2024-11-05 19:11:16 +0200

    fix: adding wp and hp to TaskItem component

--- components/TaskItem.tsx
+++ components/TaskItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export type Task = {
     id: string;
